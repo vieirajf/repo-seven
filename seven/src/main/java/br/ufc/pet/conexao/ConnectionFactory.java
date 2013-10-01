c0a2981582ac4c031eb67dd0a5d3f4b9e6f2a82c
vieirajf <vieirajf@gmail.com>
2013-10-01 12:14:00 -0300

    teste

--- seven/src/main/java/br/ufc/pet/conexao/ConnectionFactory.java
+++ seven/src/main/java/br/ufc/pet/conexao/ConnectionFactory.java
@@ -8,9 +8,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
 
